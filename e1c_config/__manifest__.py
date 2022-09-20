2be5cba75a9718f7531b919d5d37062e34ad0381
dns <felix.denis@ndp-systemes.fr>
2022-09-20 08:21:31 +0200

    starting project

---
+++ e1c_config/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "e1c_config",
+    'summary': """""",
+    'description': """""",
+    'author': "En 1 Cliché",
+    'website': "https://en1cliche.fr",
+    'category': '',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'application': False,
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'pc_erp'
+    ],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
---
+++ e1c_data/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "e1c_data",
+    'summary': """""",
+    'description': """""",
+    'author': "En 1 Cliché",
+    'website': "https://en1cliche.fr",
+    'category': '',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'application': False,
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'e1c_config'
+    ],
+
+    # always loaded
+    'data': [
+        'data/company.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
